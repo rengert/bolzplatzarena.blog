d13593215fa8a87aba9a732277a6c908eda1d019
Thomas Renger <thomas.renger@gmail.com>
2021-01-03 19:58:42 +0100

    update to .net 5 & prerelease of piranha

--- bolzplatzarena.blog/Models/Post.cs
+++ bolzplatzarena.blog/Models/Post.cs
@@ -6,7 +6,7 @@ using Piranha.Models;
 namespace Bolzplatzarena.Blog.Models
 {
 	[PostType(Title = "Post", Id = "Post")]
-	[PostTypeRoute(Title = "Post", Route = "/detail")]
+	[ContentTypeRoute(Title = "Post", Route = "/detail")]
 	public class Post : Post<Post>
 	{
 		[Region]
