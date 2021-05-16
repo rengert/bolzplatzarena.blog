b9c175ae0b762ef3808e72e254028e8fca5f672c
Thomas Renger <thomas.renger@gmail.com>
2021-05-16 17:31:18 +0200

    feat: empty seo if no content found

--- bolzplatzarena.blog/angular/src/app/components/cms/cms.component.ts
+++ bolzplatzarena.blog/angular/src/app/components/cms/cms.component.ts
@@ -37,19 +37,35 @@ export class CmsComponent {
       return;
     }
 
-    const url = document.URL; //window.location.pathname;
+    const url = document.URL;
     this.meta.updateTag({ name: 'description', content: page.description });
     this.meta.updateTag({ name: 'keywords', content: page.keywords });
     this.meta.updateTag({ name: 'robots', content: page.robots });
     // open graph
     this.meta.updateTag({ property: 'og-description', content: page.description });
-    this.meta.updateTag({ property: 'og-title', content: page.metaTitle });
+    this.meta.updateTag({ property: 'og-title', content: page.metaTitle ?? page.title });
     this.meta.updateTag({ property: 'og-url', content: url });
     // twitter
     this.meta.updateTag({ property: 'twitter:url', content: url });
-    this.meta.updateTag({ property: 'twitter:title', content: page.metaTitle });
+    this.meta.updateTag({ property: 'twitter:title', content: page.metaTitle ?? page.title });
     this.meta.updateTag({ property: 'twitter:description', content: page.description });
 
     this.title.setTitle(page.title);
+  }
+
+  private emptyMeta(): void {
+    this.meta.updateTag({ name: 'description', content: '' });
+    this.meta.updateTag({ name: 'keywords', content: '' });
+    this.meta.updateTag({ name: 'robots', content: 'follow,no-index' });
+    // open graph
+    this.meta.updateTag({ property: 'og-description', content: '' });
+    this.meta.updateTag({ property: 'og-title', content: '' });
+    this.meta.updateTag({ property: 'og-url', content: '' });
+    // twitter
+    this.meta.updateTag({ property: 'twitter:url', content: '' });
+    this.meta.updateTag({ property: 'twitter:title', content: '' });
+    this.meta.updateTag({ property: 'twitter:description', content: '' });
+
+    this.title.setTitle('Hier gibt es scheinbar gar nicht so viel zu sehen.');
   }
 }
