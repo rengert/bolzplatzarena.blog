fc871ea2cbf9d3f2c5fc1643512031e3542e187f
Thomas Renger <thomas.renger@gmail.com>
2021-05-28 18:58:22 +0200

    feat: introduce the interfaces

--- bolzplatzarena.blog/angular/src/app/components/blocks/performance-block/performance-block.component.ts
+++ bolzplatzarena.blog/angular/src/app/components/blocks/performance-block/performance-block.component.ts
@@ -1,21 +1,29 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
+interface Test {
+  name: string;
+}
+
+interface Result {
+  label: string;
+  time?: number;
+  percentage?: number;
+}
+
+
 @Component({
   selector: 'app-performance-block',
   templateUrl: './performance-block.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PerformanceBlockComponent {
-  data = [
+  test: Test = { name: 'Performance Moment' };
+  data: Result[] = [
     {
       label: 'Mein Super Test',
-      value: 10,
-      time: 123,
     },
     {
       label: 'Mein Super Test',
-      value: 45,
-      time: 1232,
     },
   ];
 }
