3c362246efd3398d5869be08b7810a4f685054e6
Thomas Renger <thomas.renger@gmail.com>
2021-06-14 18:35:22 +0200

    feat: meta ignore

--- bolzplatzarena.blog/angular/src/app/components/tags/tags.component.ts
+++ bolzplatzarena.blog/angular/src/app/components/tags/tags.component.ts
@@ -1,8 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
-import { filter, map, switchMap } from 'rxjs/operators';
+import { filter, map, switchMap, tap } from 'rxjs/operators';
+import { Page } from '../../models/page';
 import { Teaser } from '../../models/teaser';
+import { MetaDataService } from '../../services/meta-data.service';
 import { PageService } from '../../services/page.service';
 
 @Component({
@@ -13,10 +15,14 @@ import { PageService } from '../../services/page.service';
 export class TagsComponent {
   readonly posts$: Observable<Teaser[]>;
 
-  constructor(route: ActivatedRoute, page: PageService) {
+  constructor(route: ActivatedRoute, page: PageService, metaData: MetaDataService) {
     this.posts$ = route.params.pipe(
       map(data => data.tag?.toLowerCase()),
       filter(tag => tag),
+      tap(tag => metaData.update({
+        title: `Posts zum Thema: ${tag}`,
+        robots: 'follow,no-index',
+      } as Page)),
       switchMap(tag => page.archive().then(
         teasers => teasers.filter(({ tags }) => tags.some(({ title }) => title.toLowerCase() === tag)),
       )),
