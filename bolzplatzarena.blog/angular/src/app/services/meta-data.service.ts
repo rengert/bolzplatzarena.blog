19cccbcdfb734e4bf324b29564ff6b12864365ba
Thomas Renger <thomas.renger@gmail.com>
2021-06-26 17:02:19 +0200

    feat: meta extension

--- bolzplatzarena.blog/angular/src/app/services/meta-data.service.ts
+++ bolzplatzarena.blog/angular/src/app/services/meta-data.service.ts
@@ -22,14 +22,14 @@ export class MetaDataService {
     this.meta.updateTag({ name: 'robots', content: page.robots ?? 'infex,follow' });
     // open graph
     this.meta.updateTag({ property: 'og-description', content: page.description });
-    this.meta.updateTag({ property: 'og-title', content: page.metaTitle ?? page.title });
+    this.meta.updateTag({ property: 'og-title', content: `${page.metaTitle ?? page.title} - bolzplatzarena.net` });
     this.meta.updateTag({ property: 'og-url', content: url });
     // twitter
     this.meta.updateTag({ property: 'twitter:url', content: url });
     this.meta.updateTag({ property: 'twitter:title', content: page.metaTitle ?? page.title });
     this.meta.updateTag({ property: 'twitter:description', content: page.description });
 
-    this.title.setTitle(page.title);
+    this.title.setTitle(`${page.title} - bolzplatzarena.net`);
   }
 
   private emptyMeta(): void {
@@ -45,6 +45,6 @@ export class MetaDataService {
     this.meta.updateTag({ property: 'twitter:title', content: '' });
     this.meta.updateTag({ property: 'twitter:description', content: '' });
 
-    this.title.setTitle('Hier gibt es scheinbar gar nicht so viel zu sehen.');
+    this.title.setTitle('Hier gibt es scheinbar gar nicht so viel zu sehen. - bolzplatzarena.net');
   }
 }
