8a0c352f19e0e33b196c447941e83b9aafa91026
Thomas Renger <thomas.renger@gmail.com>
2021-08-09 06:49:34 +0200

    feat: play simple sound from lorem ipsum texts

--- bolzplatzarena.blog/angular/src/app/components/blocks/sound/sound.component.ts
+++ bolzplatzarena.blog/angular/src/app/components/blocks/sound/sound.component.ts
@@ -10,66 +10,52 @@ export class SoundComponent {
   }
 
   async playSound(): Promise<void> {
-    const data1 = ['ELT 1', 'MAL 2', 'MAL 6', 'TRO 2', 'DUS 2', 'XAR 2', 'TRO 1'];
-    const data2 = ['FAL 1', 'ELT 1', 'MAL 2', 'MAL 6', 'TRO 2', 'DUS 2', 'XAR 2', 'TRO 1'];
-    const data3 = ['MAL 6', 'TRO 2', 'DUS 2', 'XAR 2', 'TRO 1', 'ELT 1', 'MAL 2'];
-    const data4 = ['PUH 1', 'ELT 1', 'MAL 2', 'MAL 6', 'TRO 2', 'DUS 2', 'XAR 2', 'TRO 1'];
+    const delay = 150;
+    const types: OscillatorType[] = ['sine', 'sawtooth', 'square', 'triangle'];
+    const data1 = 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.';
+    const data2 = 'Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.';
+    const data3 = 'Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.';
+    const data4 = 'Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.';
+    const data5 = 'Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis.';
 
-    await Promise.all([
-      new Promise<void>(async(resolve) => {
+    const promises = [data1, data2, data3, data4].map(
+      (data, index) => new Promise<void>(async(resolve) => {
         setTimeout(() => {
-          this.playStringsAsSound(data1);
+          this.playStringsAsSound(data.split(' '), types[index], index);
           resolve();
-        }, 100);
+        }, index * delay);
       }),
-      new Promise<void>(async(resolve) => {
-        setTimeout(() => {
-          this.playStringsAsSound(data2);
-          resolve();
-        }, 300);
-      }),
-      new Promise<void>(async(resolve) => {
-        setTimeout(() => {
-          this.playStringsAsSound(data3);
-          resolve();
-        }, 500);
-      }),
-      new Promise<void>(async(resolve) => {
-        setTimeout(() => {
-          this.playStringsAsSound(data4);
-          resolve();
-        }, 700);
-      }),
-    ]);
+    );
+
+    await Promise.all(promises);
   }
 
-  async playStringsAsSound(data: string []): Promise<void> {
+  async playStringsAsSound(data: string [], type: OscillatorType, index: number): Promise<void> {
     const context = new AudioContext();
     const oscillator = context.createOscillator();
     const gain = context.createGain();
-    oscillator.type = 'sine';
+    oscillator.type = type;
     oscillator.connect(gain);
     gain.connect(context.destination);
 
     oscillator.start();
 
     for (const term of data) {
+      gain.gain.exponentialRampToValueAtTime(1, context.currentTime);
       let frequency = 0;
       for (let i = 0; i < term.length; i++) {
-        frequency += term.charCodeAt(i);
+        frequency += 2 * term.charCodeAt(i);
       }
-      console.log(term, frequency);
+      console.log(index, term, frequency);
       try {
         oscillator.frequency.value = frequency;
+        gain.gain.exponentialRampToValueAtTime(0.00001, context.currentTime + 2.45);
       } catch {
       }
 
       await new Promise<void>((resolve) => {
-        setTimeout(() => resolve(), 200);
+        setTimeout(() => resolve(), 2600);
       });
     }
-    gain.gain.exponentialRampToValueAtTime(
-      0.00001, context.currentTime + 2.5,
-    );
   }
 }
