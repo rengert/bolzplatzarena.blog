96817a12cb91c85f6c729ec688d5fabe870b734a
Thomas Renger <thomas.renger@gmail.com>
2021-08-21 11:06:28 +0200

    feat: add sound to cms

---
+++ bolzplatzarena.blog/Blocks/SoundBlock.cs
@@ -0,0 +1,9 @@
+using Piranha.Extend;
+
+namespace Bolzplatzarena.Blog.Blocks
+{
+	[BlockType(Name = "Sound", Category = "Content", Icon = "fas fa-music")]
+	public class SoundBlock : Block
+	{
+	}
+}
