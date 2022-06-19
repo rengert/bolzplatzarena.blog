6c32aa3b26dfd118866ec934d8f750fb043115e2
Thomas Renger <thomas.renger@gmail.com>
2022-06-19 12:04:03 +0200

    feat: clean page design

---
+++ bolzplatzarena.blog/Models/CleanPage.cs
@@ -0,0 +1,10 @@
+using Piranha.AttributeBuilder;
+using Piranha.Models;
+
+namespace Bolzplatzarena.Blog.Models
+{
+	[PageType(Title = "Clean page")]
+	public class CleanPage : Page<CleanPage>
+	{
+	}
+}
