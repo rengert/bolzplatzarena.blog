5c5f2e65e88367b9346031125e390a846f18530a
Thomas Renger <thomas.renger@gmail.com>
2023-02-12 16:21:34 +0100

    feat: refactor the projects section

--- bolzplatzarena.blog/angular/src/app/app.component.ts
+++ bolzplatzarena.blog/angular/src/app/app.component.ts
@@ -5,6 +5,12 @@ import { filter, switchMap } from 'rxjs/operators';
 import { AppContextService } from './services/app-context.service';
 import { FeedbackService } from './services/feedback.service';
 
+interface Project {
+  title: string;
+  url: string;
+  description: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -12,6 +18,23 @@ import { FeedbackService } from './services/feedback.service';
 })
 export class AppComponent {
   readonly versionUpdate = new BehaviorSubject(false);
+  readonly projects: Project[] = [
+    {
+      title: 'Pendlergeschichten - "Auch Lehrer fahren Zug - Leider!"',
+      url: 'https://www.pendlergeschichten.de/',
+      description: 'Auszüge aus dem Buch "Auch Lehrer fahren Zug - Leider!"',
+    },
+    {
+      title: 'Frühstück in und um Karlsruhe',
+      url: 'https://www.fruehstueck-karlsruhe.de',
+      description: 'Eine Übersicht über Frühstücksmöglichkeiten in und um Karlsruhe',
+    },
+    {
+      title: 'Bürgerenergie Karlsruhe',
+      url: 'https://ben-karlsruhe.de/',
+      description: 'Die Bürgerenergie Karlsruhe ist eine Genossenschaft, die sich für die Energiewende in Karlsruhe einsetzt.',
+    },
+  ];
 
   constructor(update: SwUpdate, feedback: FeedbackService, readonly app: AppContextService) {
     update.available.subscribe(() => {
