d2e7cb26030ae97a5387f29407087ca4c5846439
Thomas Renger <thomas.renger@gmail.com>
2023-02-16 20:44:21 +0100

    fix: do not request homepage twice

--- bolzplatzarena.blog/angular/src/app/services/content.service.ts
+++ bolzplatzarena.blog/angular/src/app/services/content.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
 import { Observable, shareReplay } from 'rxjs';
-import { filter, map, startWith, switchMap, tap } from 'rxjs/operators';
+import { filter, map, switchMap, tap } from 'rxjs/operators';
 import { Page } from '../models/page';
 import { MetaDataService } from './meta-data.service';
 import { PageService } from './page.service';
@@ -22,7 +22,6 @@ export class ContentService {
     this.data$ = router.events.pipe(
       filter(event => event instanceof NavigationEnd),
       map(event => (event as NavigationEnd).url),
-      startWith(router.url),
       switchMap(url => page.bySlug(url)),
       tap(page => metaData.update(page)),
       map(page => ({ page })),
