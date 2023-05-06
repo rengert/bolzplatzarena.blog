865dfd7167087b942e08af43bf84242e098d2c60
Thomas Renger <thomas.renger@gmail.com>
2023-05-06 13:26:28 +0200

    fix: remove settings

--- bolzplatzarena.blog/angular/src/app/modules/nonogramm/nonogramm.module.ts
+++ bolzplatzarena.blog/angular/src/app/modules/nonogramm/nonogramm.module.ts
@@ -1,28 +1,24 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BlockComponent } from './components/game/board/block/block.component';
+import { BoardComponent } from './components/game/board/board.component';
+import { CaptionComponent } from './components/game/board/caption/caption.component';
+import { HeartsComponent } from './components/game/board/hearts/hearts.component';
 import { GameComponent } from './components/game/game.component';
 import { NonogrammComponent } from './components/nonogramm.component';
 import { SettingsComponent } from './components/settings/settings.component';
-
 import { NonogrammRoutingModule } from './nonogramm-routing.module';
-import { BoardComponent } from './components/game/board/board.component';
-import { BlockComponent } from './components/game/board/block/block.component';
-import { CaptionComponent } from './components/game/board/caption/caption.component';
-import { HeartsComponent } from './components/game/board/hearts/hearts.component';
-import { SettingsViewComponent } from './components/game/board/settings-view/settings-view.component';
-
 
 @NgModule({
   declarations: [
-    NonogrammComponent,
-    GameComponent,
-    SettingsComponent,
-    BoardComponent,
     BlockComponent,
+    BoardComponent,
     CaptionComponent,
+    GameComponent,
     HeartsComponent,
-    SettingsViewComponent,
+    NonogrammComponent,
+    SettingsComponent,
   ],
   imports: [
     CommonModule,
