a88829779cf14976bf7cf3e918739cdfca9b98a3
Thomas Renger <thomas.renger@gmail.com>
2023-06-24 00:05:59 +0200

    refactor: using signals in header

--- bolzplatzarena.blog/angular/src/app/components/header/header.component.ts
+++ bolzplatzarena.blog/angular/src/app/components/header/header.component.ts
@@ -1,6 +1,6 @@
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Signal, signal } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
 import { NavigationEnd, Router } from '@angular/router';
-import { Observable } from 'rxjs';
 import { filter, map } from 'rxjs/operators';
 import { PageType } from '../../models/page-type.enum';
 import { ContentService } from '../../services/content.service';
@@ -12,20 +12,18 @@ import { ContentService } from '../../services/content.service';
 })
 export class HeaderComponent {
   readonly navigationOpen = signal(false);
+  readonly title: Signal<string | undefined>;
 
-  readonly title$: Observable<string | undefined>;
-
-  constructor(content: ContentService, router: Router) {
+  constructor(private readonly content: ContentService, router: Router) {
     router.events.pipe(
       filter(event => event instanceof NavigationEnd),
     ).subscribe(() => this.navigationOpen.set(false));
-
-    this.title$ = content.data$.pipe(
+    this.title = toSignal(this.content.data$.pipe(
       map(data => (data.page?.type === PageType.Post)
         ? undefined
         : data.page?.title,
       ),
-    );
+    ));
   }
 
   toggleNavigation() {
