9507d1dff88fb7fae34174968d77d647d7194c40
Thomas Renger <thomas.renger@gmail.com>
2023-09-30 16:42:37 +0200

    refactor: migrate to standalone app

--- bolzplatzarena.blog/angular/src/app/app.component.ts
+++ bolzplatzarena.blog/angular/src/app/app.component.ts
@@ -4,6 +4,13 @@ import { timer } from 'rxjs';
 import { filter, switchMap } from 'rxjs/operators';
 import { AppContextService } from './services/app-context.service';
 import { FeedbackService } from './services/feedback.service';
+import { FooterComponent } from './components/footer/footer.component';
+import { TagCloudComponent } from './components/tag-cloud/tag-cloud.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { NgFor, NgIf } from '@angular/common';
+import { SectionHeaderComponent } from './components/section-header/section-header.component';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
 
 interface Project {
   title: string;
@@ -30,9 +37,20 @@ const projects: Project[] = [
 ];
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    standalone: true,
+    imports: [
+        HeaderComponent,
+        RouterOutlet,
+        SectionHeaderComponent,
+        NgFor,
+        NavigationComponent,
+        TagCloudComponent,
+        NgIf,
+        FooterComponent,
+    ],
 })
 export class AppComponent {
   protected readonly versionUpdate = signal(false);
--- bolzplatzarena.blog/angular/src/main.ts
+++ bolzplatzarena.blog/angular/src/main.ts
@@ -1,13 +1,27 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { NgOptimizedImage } from '@angular/common';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { enableProdMode, importProvidersFrom, LOCALE_ID } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { bootstrapApplication, BrowserModule } from '@angular/platform-browser';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { AppRoutingModule } from './app/app-routing.module';
+import { AppComponent } from './app/app.component';
 
-import { AppModule } from './app/app.module';
+
 import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic()
-  .bootstrapModule(AppModule)
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(BrowserModule, AppRoutingModule, ReactiveFormsModule, ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      registrationStrategy: 'registerWhenStable:30000',
+    }), FormsModule, NgOptimizedImage),
+    { provide: LOCALE_ID, useValue: 'de' },
+    provideHttpClient(withInterceptorsFromDi()),
+  ],
+})
   .catch(err => console.error(err));
